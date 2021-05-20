abf6e18b7067010055b36257b4c73ffe8add6d49
Jon Lea <jlea@ucdavis.edu>
2021-05-20 11:07:41 -0700

    B-def

--- WebApplication2/Default.aspx.cs
+++ WebApplication2/Default.aspx.cs
@@ -8,7 +8,7 @@ using System.Web.UI.WebControls;
 namespace WebApplication2 {
 	public partial class _Default : Page {
 		protected void Page_Load(object sender, EventArgs e) {
-
+			int i = 0;
 		}
 	}
 }
