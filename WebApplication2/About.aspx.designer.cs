2a9fcab7f15bf030b2c87308afc44faefc610089
Jon Lea <jlea@ucdavis.edu>
2021-06-01 15:39:23 -0700

    change for branch 3

--- WebApplication2/About.aspx.designer.cs
+++ WebApplication2/About.aspx.designer.cs
@@ -13,12 +13,12 @@ namespace WebApplication2 {
 	public partial class About {
 
 		/// <summary>
-		/// abcLabel_ control.
+		/// abcLabel_4 control.
 		/// </summary>
 		/// <remarks>
 		/// Auto-generated field.
 		/// To modify move field declaration from designer file to code-behind file.
 		/// </remarks>
-		protected global::System.Web.UI.WebControls.Label abcLabel_;
+		protected global::System.Web.UI.WebControls.Label abcLabel_4;
 	}
 }
