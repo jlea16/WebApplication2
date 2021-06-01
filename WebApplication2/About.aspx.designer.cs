c02c74d8b69b7a2f78807924ad36c72efac3c7be
Jon Lea <jlea@ucdavis.edu>
2021-06-01 15:21:35 -0700

    123

--- WebApplication2/About.aspx.designer.cs
+++ WebApplication2/About.aspx.designer.cs
@@ -13,12 +13,12 @@ namespace WebApplication2 {
 	public partial class About {
 
 		/// <summary>
-		/// abcLabel control.
+		/// abcLabel_ control.
 		/// </summary>
 		/// <remarks>
 		/// Auto-generated field.
 		/// To modify move field declaration from designer file to code-behind file.
 		/// </remarks>
-		protected global::System.Web.UI.WebControls.Label abcLabel;
+		protected global::System.Web.UI.WebControls.Label abcLabel_;
 	}
 }
